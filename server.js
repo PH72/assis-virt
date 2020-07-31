d93eb98793cf8b79039515bde8eb758a645ad26f
Glitch (hello-express) <none>
2020-07-31 17:21:59 +0000

    🌆🍗 Checkpoint ./server.js:20063446/876

--- server.js
+++ server.js
@@ -16,26 +16,40 @@ app.get('/', (request, response) => {
 });
 
 app.post("/glpi", async (request, response) => {
-  var user = new UserGlpi(request.body.queryResult.parameters['Login'],request.body.queryResult.parameters['Senha'],request.headers['app-token']);
-  var ticket = {
-    name: request.body.queryResult.parameters['Titulo'],
-    content: request.body.queryResult.parameters['Ticket']
-  };
   
-  //Inicia sessão do usuário
-  await user.initSession();
-  //Verfica se foi iniciado corretamente
-  if(user.errorLogin != undefined && user.errorLogin != null){
-    response.json({"fulfillmentText":""+user.errorLogin.message});
-  }
-  //Cria o chamado
-  await user.createTicket(ticket.name,ticket.content);
-  //Verifica se o chamado foi criado corretamente
-  if(user.errorCreateTicket != undefined && user.errorCreateTicket != null){
-    response.json({"fulfillmentText":""+user.errorCreateTicket.message});
+  var intentName = request.body.queryResult.intent.displayName;
+  
+  if (intentName == "faq.TI"){
+        var user = new UserGlpi(request.body.queryResult.parameters['Login'],request.body.queryResult.parameters['Senha'],request.headers['app-token']);
+        var ticket = {
+          name: request.body.queryResult.parameters['Titulo'],
+          content: request.body.queryResult.parameters['Ticket']
+        };
+
+        //Inicia sessão do usuário
+        await user.initSession();
+        //Verfica se foi iniciado corretamente
+        if(user.errorLogin != undefined && user.errorLogin != null){
+          response.json({"fulfillmentText":""+user.errorLogin.message});
+        }
+        //Cria o chamado
+        await user.createTicket(ticket.name,ticket.content);
+        //Verifica se o chamado foi criado corretamente
+        if(user.errorCreateTicket != undefined && user.errorCreateTicket != null){
+          response.json({"fulfillmentText":""+user.errorCreateTicket.message});
+        }
+
+        response.json({"fulfillmentText":"Chamado criado com sucesso! id: "+user.ticketCreated.id+"."});
   }
   
-  response.json({"fulfillmentText":"Chamado criado com sucesso! id: "+user.ticketCreated.id+"."});
+  else if (intentName == "problemas.rede - Caiu - Resolvido"){
+    
+    response.jason({"fulfillmenteText":"Problema resolvido"})
+    
+  }
+  
+  
+  
 });
 
 const listener = app.listen(process.env.PORT, () => {
