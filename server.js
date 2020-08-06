d98c4d764f826dfcbe03d7cca4ce0d454ec4d916
Glitch (hello-express) <none>
2020-08-06 19:22:25 +0000

    👁⛲️ Checkpoint ./server.js:20063446/1862

--- server.js
+++ server.js
@@ -48,8 +48,6 @@ app.post("/glpi", async (request, response) => {
     [
      {
        "card": {
-         "title": "card title",
-         "subtitle": "card text",
          "imageUri": "https://firebasestorage.googleapis.com/v0/b/webhook-9a993.appspot.com/o/Tutoriais%2FSem%20t%C3%ADtulo%201.png?alt=media&token=02deb325-3ea9-4b7a-bc27-69e6a0318b46"
         
         
@@ -59,11 +57,35 @@ app.post("/glpi", async (request, response) => {
     {
       "text": {
          "text": [
-          "Primeiro Passo: Verifique se o seu computador está conectado a internet, va até o canto inferior esquerdo da tela onde você verá um ícone referente a rede \\n",
-          "teste"
+          "Primeiro Passo: Verifique se o seu computador está conectado a internet, va até o canto inferior esquerdo da tela onde você verá um ícone referente a rede, como mostradono Passo 1 na figura."
                  ]
-             }
-    }
+          }
+    },
+      
+    {
+      "text": {
+         "text": [
+          "Segundo Passo: Se seu computador estiver conectado via rede cabeada você ira visualizar exatamente oque o Passo 2 mostra."
+                 ]
+          }
+    },
+      
+    {
+      "text": {
+         "text": [
+          "Terceiro Passo: Caso seu computador não esteja cabeado será necessario ultilizar a Rede WI-FI, no Passo 3 da imagem podemos visualizar uma lista de redes disponiveis, por padrão aqui na Febracis", 
+          "ultilizamos a rede (Interno) que fica acessivel com a senha (F3br@c152017)."
+                 ]
+          }
+    },
+      
+    {
+      "text": {
+         "text": [
+          "Primeiro Passo: Verifique se o seu computador está conectado a internet, va até o canto inferior esquerdo da tela onde você verá um ícone referente a rede."
+                 ]
+          }
+    }  
   
   ]
       
