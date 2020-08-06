0f47b23fcfb2bfbcdcefd1e8e4d213132f08c5c4
Glitch (hello-express) <none>
2020-08-06 18:58:24 +0000

    🚞🏇 Checkpoint ./server.js:20063446/1027

--- server.js
+++ server.js
@@ -44,34 +44,34 @@ app.post("/glpi", async (request, response) => {
   
   else if (intentName == "Problema.Internet - sem.Internet"){
     
-    response.json({"fulfillmentMessages": [
-    {
-      "card": {
-        "title": "card title",
-        "subtitle": "card text",
-        "imageUri": "https://firebasestorage.googleapis.com/v0/b/webhook-9a993.appspot.com/o/Tutoriais%2FSem%20t%C3%ADtulo%201.png?alt=media&token=02deb325-3ea9-4b7a-bc27-69e6a0318b46",
-        "text": {
-          "text": [
-            "Primeiro Passo: Verifique se o seu computador está conectado a internet, va até o canto inferior esquerdo da tela onde você verá um ícone referente a rede \n\n",
-            "teste"
-          ]
-        }
+    response.json({"fulfillmentMessages": 
+    [
+     {
+       "card": {
+         "title": "card title",
+         "subtitle": "card text",
+         "imageUri": "https://firebasestorage.googleapis.com/v0/b/webhook-9a993.appspot.com/o/Tutoriais%2FSem%20t%C3%ADtulo%201.png?alt=media&token=02deb325-3ea9-4b7a-bc27-69e6a0318b46"
         
-      }
+        
+              }
+    },
+  
+    {
+      "text": {
+         "text": [
+          "Primeiro Passo: Verifique se o seu computador está conectado a internet, va até o canto inferior esquerdo da tela onde você verá um ícone referente a rede \n\n",
+          "teste"
+                 ]
+             }
     }
+  
   ]
-   })
-   /*response.json({"fulfillmentMessages": [
-      {
-        "text": {
-          "text": [
-            "Primeiro Passo: Verifique se o seu computador está conectado a internet, va até o canto inferior esquerdo da tela onde você verá um ícone referente a rede \n\n",
-            "teste"
-          ]
-        }
-      }
-    ]
-  })*/ 
+      
+})
+   
+      
+    
+  
     
  }
   
