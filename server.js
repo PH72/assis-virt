3f3556aba301b3128956f0d8e711875a78fa4329
Glitch (hello-express) <none>
2020-08-06 19:34:23 +0000

    🚋🖼 Checkpoint ./server.js:20063446/209

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post("/glpi", async (request, response) => {
     {
       "text": {
          "text": [
-          "Primeiro Passo: Verifique se o seu computador está conectado a internet, va até o canto inferior esquerdo da tela onde você verá um ícone referente a rede."
+          "Após realizar os Procedimentos abaixo me infome se você conseguiu ou não resolver o problema."
                  ]
           }
     }  
