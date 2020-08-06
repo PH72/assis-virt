b9c1de5e9d67ac9e87bad40a9ad826e51d44e01b
Glitch (hello-express) <none>
2020-08-06 18:25:10 +0000

    🏦✈️ Checkpoint ./server.js:20063446/769

--- server.js
+++ server.js
@@ -44,18 +44,28 @@ app.post("/glpi", async (request, response) => {
   
   else if (intentName == "Problema.Internet - sem.Internet"){
     
-    response.json({
-  "fulfillmentMessages": [
+    response.json({"fulfillmentMessages": [
     {
       "card": {
         "title": "card title",
         "subtitle": "card text",
-        "imageUri": "https://firebasestorage.googleapis.com/v0/b/webhook-9a993.appspot.com/o/Tutoriais%2FSem%20t%C3%ADtulo.png?alt=media&token=51b1f74b-2000-4f7c-ab9f-3baad59eba8d"
+        "imageUri": "https://firebasestorage.googleapis.com/v0/b/webhook-9a993.appspot.com/o/Tutoriais%2FSem%20t%C3%ADtulo%201.png?alt=media&token=02deb325-3ea9-4b7a-bc27-69e6a0318b46"
         
       }
     }
   ]
-})
+   })
+   response.json({
+    "fulfillmentMessages": [
+      {
+        "text": {
+          "text": [
+            "Primeiro Passo: Verifique se o seu computador está conectado a internet "
+          ]
+        }
+      }
+    ]
+  }) 
     
     }
   
