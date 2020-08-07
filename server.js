4ba535e2e607bc103dc866526c42d75900d8564b
Glitch (hello-express) <none>
2020-08-07 19:51:52 +0000

    🐵🎂 Checkpoint ./server.js:20063446/1731

--- server.js
+++ server.js
@@ -102,7 +102,17 @@ app.post("/glpi", async (request, response) => {
   
   else if (intentName == "Problema.Internet - Internet.Lenta"){
     
-    
+   response.json({"fulfillmentMessages":
+     [
+       
+        {
+         "card": {
+           "imageUri": "https://firebasestorage.googleapis.com/v0/b/webhook-9a993.appspot.com/o/Tutoriais%2FSem%20t%C3%ADtulo%201.png?alt=media&token=02deb325-3ea9-4b7a-bc27-69e6a0318b46"
+                 }
+        } 
+     
+     ] 
+   }) 
     
     
     
