3df1f4a47a32493c6aa488fad115315c6084f053
Glitch (hello-express) <none>
2020-09-02 21:54:59 +0000

    🍋💸 Checkpoint ./server.js:20063446/4624

--- server.js
+++ server.js
@@ -3,11 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
-
 const UserGlpi = require("./modelUser.js");
-
 const app = express();
-
 const {WebhookClient} = require('dialogflow-fulfillment');
 
 app.use(bodyParser.json())
@@ -25,20 +22,20 @@ app.post("/glpi", async (request, response) => {
   
   
   
-  if (intentName == "Internet_lenta_não_resolvido"||"Impressora_não_Instalada"){
+  
       let intentMap = new Map();
       
       intentMap.set('Internet_lenta_não_resolvido',Abre_Chamados);
       intentMap.set('Impressora_não_Instalada',Abre_Chamados);
+      agent.handleRequest(intentMap);
  
     
     function Abre_Chamados(agent){
-          agent.handleRequest(intentMap);
-          agent.add("");
-          agent.setFollowupEvent('teste');
 
-        }
-  }
+          agent.add("olá");
+          //agent.setFollowupEvent('teste');
+      
+    }
   
   
   
@@ -99,81 +96,6 @@ app.post("/glpi", async (request, response) => {
   
   
   
-  else if (intentName == "Problema.Internet - sem.Internet"){
-    
-    response.json({"fulfillmentMessages": 
-    [
-     {
-       "card": {
-         "imageUri": "https://firebasestorage.googleapis.com/v0/b/webhook-9a993.appspot.com/o/Tutoriais%2FSem%20t%C3%ADtulo%201.png?alt=media&token=02deb325-3ea9-4b7a-bc27-69e6a0318b46"
-        
-        
-              }
-    },
-  
-    {
-      "text": {
-         "text": [
-          "Primeiro Passo: Verifique se o seu computador está conectado a internet, va até o canto inferior esquerdo da tela onde você verá um ícone referente a rede, como mostradono Passo 1 na figura."
-                 ]
-          }
-    },
-      
-    {
-      "text": {
-         "text": [
-          "Segundo Passo: Se seu computador estiver conectado via rede cabeada você ira visualizar exatamente oque o Passo 2 mostra."
-                 ]
-          }
-    },
-      
-    {
-      "text": {
-         "text": [
-          "Terceiro Passo: Caso seu computador não esteja cabeado será necessario ultilizar a Rede WI-FI, no Passo 3 da imagem podemos visualizar uma lista de redes disponiveis, por padrão aqui na Febracis", 
-          "ultilizamos a rede (Interno) que fica acessivel com a senha (F3br@c152017)."
-                 ]
-          }
-    },
-      
-    {
-      "text": {
-         "text": [
-          "Após realizar os Procedimentos abaixo me infome se você conseguiu ou não resolver o problema."
-                 ]
-          }
-    }  
-  
-  ]
-      
-})
-   
-      
-    
-  
-    
- }
-  
-  
-  else if (intentName == "Problema.Internet - Internet.Lenta"){
-    
-   response.json({"fulfillmentMessages":
-     [
-       
-        {
-         "card": {
-           "imageUri": "https://firebasestorage.googleapis.com/v0/b/webhook-9a993.appspot.com/o/Tutoriais%2FSem%20t%C3%ADtulo%201.png?alt=media&token=02deb325-3ea9-4b7a-bc27-69e6a0318b46"
-                 }
-        } 
-     
-     ] 
-   }) 
-    
-    
-    
-  }
-  
-  //response.json({"fulfillmentText":intentName})
   
 });
 
